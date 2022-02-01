538c5c8d3730e068fd8ffc4a818940bd3658ff83
Jaziel Isaí <92839539+JazielIsai@users.noreply.github.com>
2022-02-01 00:46:24 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+      <div>
+          <Header /> 
+          <Nav />
+      </div>
+      
+      <Main />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
